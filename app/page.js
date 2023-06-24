10b6b0599986a2017e57f18896e5920637cebf74
Adithya <jhadith88@gmail.com>
2023-06-24 16:54:24 +0530

    done some animation

--- app/page.js
+++ app/page.js
@@ -1,113 +1,169 @@
-import Image from 'next/image'
+'use client';
+import Image from 'next/image';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
+
+// import { useState, useEffect } from 'react';
+
+// import { Orbitron } from 'next/font/google';
+
+const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i) => {
+    const delay = 1 + i * 0.5;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: 'spring', duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    };
+  },
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap"
+          rel="stylesheet"
         />
+      </Head>
+
+      <main className="flex justify-center">
+        <div className="mt-[100px]">
+          <motion.div
+            // animate={{ x: 100 }}
+            whileInView={{ y: 20 }}
+            transition={{ delay: 4 }}
+          >
+            STORIES.GPT
+          </motion.div>
+        </div>
+      </main>
+
+      <div className="ml-[-350px]">
+        <motion.svg
+          width="600"
+          height="600"
+          viewBox="0 0 500 600"
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.circle
+            cx="100"
+            cy="100"
+            r="80"
+            stroke="#ff5554"
+            variants={draw}
+            custom={1}
+          />
+          <motion.line
+            x1="220"
+            y1="30"
+            x2="360"
+            y2="170"
+            stroke="#ffffff"
+            variants={draw}
+            custom={2}
+          />
+          <motion.line
+            x1="220"
+            y1="170"
+            x2="360"
+            y2="30"
+            stroke="#00cc88"
+            variants={draw}
+            custom={2.5}
+          />
+          <motion.rect
+            width="140"
+            height="140"
+            x="410"
+            y="30"
+            rx="20"
+            stroke="#87CEEB"
+            variants={draw}
+            custom={3}
+          />
+          <motion.circle
+            cx="100"
+            cy="300"
+            r="80"
+            stroke="#000000"
+            variants={draw}
+            custom={2}
+          />
+          <motion.line
+            x1="220"
+            y1="230"
+            x2="360"
+            y2="370"
+            stroke="#ff0055"
+            custom={3}
+            variants={draw}
+          />
+          <motion.line
+            x1="220"
+            y1="370"
+            x2="360"
+            y2="230"
+            stroke="#ff0055"
+            custom={3.5}
+            variants={draw}
+          />
+          <motion.rect
+            width="140"
+            height="140"
+            x="410"
+            y="230"
+            rx="20"
+            stroke="#F4C2C2"
+            custom={4}
+            variants={draw}
+          />
+          <motion.circle
+            cx="100"
+            cy="500"
+            r="80"
+            stroke="#ffffff"
+            variants={draw}
+            custom={3}
+          />
+          <motion.line
+            x1="220"
+            y1="430"
+            x2="360"
+            y2="570"
+            stroke="#ffffff"
+            variants={draw}
+            custom={4}
+          />
+          <motion.line
+            x1="220"
+            y1="570"
+            x2="360"
+            y2="430"
+            stroke="#ffffff"
+            variants={draw}
+            custom={4.5}
+          />
+          <motion.rect
+            width="140"
+            height="140"
+            x="410"
+            y="430"
+            rx="20"
+            stroke="#ffffff"
+            variants={draw}
+            custom={5}
+          />
+        </motion.svg>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
