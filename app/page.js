77f67f6b05d62769e27cd58c981801bd406166a8
Adithya <jhadith88@gmail.com>
2023-06-24 20:50:09 +0530

    done some animations

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import Head from 'next/head';
 import { motion } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 // import { useState, useEffect } from 'react';
 
@@ -23,6 +24,26 @@ const draw = {
 };
 
 export default function Home() {
+  const textRef = useRef(null);
+  useEffect(() => {
+    const textElement = textRef.current;
+    const text = textElement.textContent;
+    const textLength = text.length;
+    const typingSpeed = 100; // Adjust typing speed (in milliseconds)
+
+    let i = 0;
+    textElement.textContent = '';
+
+    const type = () => {
+      if (i < textLength) {
+        textElement.textContent += text.charAt(i);
+        i++;
+        setTimeout(type, typingSpeed);
+      }
+    };
+
+    type();
+  }, []);
   return (
     <>
       <Head>
@@ -34,19 +55,144 @@ export default function Home() {
         />
       </Head>
 
-      <main className="flex justify-center">
+      <main className="flex flex-col justify-center">
         <div className="mt-[100px]">
           <motion.div
             // animate={{ x: 100 }}
-            whileInView={{ y: 20 }}
-            transition={{ delay: 4 }}
+            whileInView={{ y: 30 }}
+            transition={{ delay: 1 }}
           >
             STORIES.GPT
           </motion.div>
+
+          <div className="mt-[60px] typingAnimation">
+            <span ref={textRef} className="typingText w-[1000px]">
+              Unleash your imagination with AI.
+            </span>
+          </div>
         </div>
       </main>
 
-      <div className="ml-[-350px]">
+      <div className="ml-[-350px] mb-[100px] z-[-2] absolute top-[80px]">
+        <motion.svg
+          width="600"
+          height="600"
+          viewBox="0 0 500 600"
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.circle
+            cx="100"
+            cy="100"
+            r="80"
+            stroke="#ff5554"
+            variants={draw}
+            custom={1}
+          />
+          <motion.line
+            x1="220"
+            y1="30"
+            x2="360"
+            y2="170"
+            stroke="#ffffff"
+            variants={draw}
+            custom={2}
+          />
+          <motion.line
+            x1="220"
+            y1="170"
+            x2="360"
+            y2="30"
+            stroke="#00cc88"
+            variants={draw}
+            custom={2.5}
+          />
+          <motion.rect
+            width="140"
+            height="140"
+            x="410"
+            y="30"
+            rx="20"
+            stroke="#87CEEB"
+            variants={draw}
+            custom={3}
+          />
+          <motion.circle
+            cx="100"
+            cy="300"
+            r="80"
+            stroke="#000000"
+            variants={draw}
+            custom={2}
+          />
+          <motion.line
+            x1="220"
+            y1="230"
+            x2="360"
+            y2="370"
+            stroke="#ff0055"
+            custom={3}
+            variants={draw}
+          />
+          <motion.line
+            x1="220"
+            y1="370"
+            x2="360"
+            y2="230"
+            stroke="#ff0055"
+            custom={3.5}
+            variants={draw}
+          />
+          <motion.rect
+            width="140"
+            height="140"
+            x="410"
+            y="230"
+            rx="20"
+            stroke="#F4C2C2"
+            custom={4}
+            variants={draw}
+          />
+          <motion.circle
+            cx="100"
+            cy="500"
+            r="80"
+            stroke="#ffffff"
+            variants={draw}
+            custom={3}
+          />
+          <motion.line
+            x1="220"
+            y1="430"
+            x2="360"
+            y2="570"
+            stroke="#ffffff"
+            variants={draw}
+            custom={4}
+          />
+          <motion.line
+            x1="220"
+            y1="570"
+            x2="360"
+            y2="430"
+            stroke="#ffffff"
+            variants={draw}
+            custom={4.5}
+          />
+          <motion.rect
+            width="140"
+            height="140"
+            x="410"
+            y="430"
+            rx="20"
+            stroke="#ffffff"
+            variants={draw}
+            custom={5}
+          />
+        </motion.svg>
+      </div>
+
+      <div className="ml-[1110px] mb-[100px] z-[-2] fixed top-[80px]">
         <motion.svg
           width="600"
           height="600"
